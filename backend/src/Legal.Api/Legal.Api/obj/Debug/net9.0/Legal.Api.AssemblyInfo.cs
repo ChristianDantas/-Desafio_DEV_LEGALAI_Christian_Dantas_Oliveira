f95f793592453b54b41d854263d524a448e52d94
Christian Dantas <73191709+ChristianDantas@users.noreply.github.com>
2025-04-17 18:53:11 -0300

    att

--- backend/src/Legal.Api/Legal.Api/obj/Debug/net9.0/Legal.Api.AssemblyInfo.cs
+++ backend/src/Legal.Api/Legal.Api/obj/Debug/net9.0/Legal.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Legal.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec2ba86f2e15b7b830b90db7a0e282de6c339276")]
 [assembly: System.Reflection.AssemblyProductAttribute("Legal.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Legal.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
